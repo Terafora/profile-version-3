c87af0a403c0d3e81a9374d446493c9b8c638524
Charlotte Stone <144109245+Terafora@users.noreply.github.com>
2024-07-16 16:17:55 +0100

    Rename projects cards

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 
-const Card = ({ project }) => {
+const ProjectCard = ({ project }) => {
     const { t } = useTranslation();
 
     return (
@@ -14,4 +14,4 @@ const Card = ({ project }) => {
     );
 };
 
-export default Card;
+export default ProjectCard;
