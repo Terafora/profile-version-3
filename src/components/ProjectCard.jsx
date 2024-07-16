6b57c67f14bf9e7e41ef2c798c3508147962ce0a
Charlotte Stone <144109245+Terafora@users.noreply.github.com>
2024-07-16 21:27:21 +0100

    Implement temporary changes to card images

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -6,7 +6,7 @@ const ProjectCard = ({ project }) => {
 
     return (
         <div className="card col-lg-3 col-md-6 col-sm-9">
-            <img src={project.imgSrc} className="Recent-Work-Img" alt={project.altText} />
+            <img src={project.imgSrc} className="Recent-Work-Img card-img" alt={project.altText} />
             <h4 className="text-center">{t(`projects.${project.id}.title`)}</h4>
             <p>{t(`projects.${project.id}.description`)}</p>
             <a href={project.url} className="btn btn-primary" target='_blank' rel="noreferrer">{t('learnMore')}</a>
--- src/components/TranslationCard.jsx
+++ src/components/TranslationCard.jsx
@@ -6,7 +6,7 @@ const TranslationCard = ({ translation }) => {
 
     return (
         <div className="card col-lg-3 col-md-6 col-sm-9">
-            <img src={translation.imgSrc} className="Recent-Work-Img" alt={translation.altText} />
+            <img src={translation.imgSrc} className="Recent-Work-Img card-img" alt={translation.altText} />
             <h4 className="text-center">{t(`translations.${translation.id}.title`)}</h4>
             <a href={translation.url} className="btn btn-primary" target='_blank' rel="noreferrer">{t('learnMore')}</a>
         </div>
