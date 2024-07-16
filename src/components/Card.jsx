168634839e94a76562ee08f4e6863981705e7918
Charlotte Stone <144109245+Terafora@users.noreply.github.com>
2024-07-16 12:49:56 +0100

    Move cards into their own component

---
+++ src/components/Card.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+
+const Card = ({ project }) => {
+    const { t } = useTranslation();
+
+    return (
+        <div className="card col-lg-3 col-md-6 col-sm-9">
+            <img src={project.imgSrc} className="Recent-Work-Img" alt={project.altText} />
+            <h4 className="text-center">{t(`projects.${project.id}.title`)}</h4>
+            <p>{t(`projects.${project.id}.description`)}</p>
+            <a href={project.url} className="btn btn-primary">{t('learnMore')}</a>
+        </div>
+    );
+};
+
+export default Card;
