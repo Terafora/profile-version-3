8648a62587f1d1a4412cd4360aac24c9770e98ca
Charlotte Stone <charlie.stone649@gmail.com>
2024-07-16 19:05:41 +0100

    Update layout of items per screen size

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,12 +8,12 @@ export default function About() {
         <section className="row about">
             <h2>{t('about')}</h2>
             <div className="row g-5 mx-2">
-                <div className="col-6">
+                <div className="col-12 col-lg-6">
                     <h3>{t('hello')}</h3>
                     <p>{t('aboutText1')}</p>
                     <p>{t('aboutText2')}</p>
                 </div>
-                <div className="col-6">
+                <div className="col-12 col-lg-6">
                     <img src="#" className="img-fluid" alt="Charlotte Stone" />
                 </div>
             </div>
