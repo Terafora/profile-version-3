eca1987e5d1dec1a064fd0411a989a97b77c218b
Charlotte Stone <144109245+Terafora@users.noreply.github.com>
2024-07-16 15:39:46 +0100

    Update styling on main content

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -9,9 +9,10 @@ export default function MainContent() {
             <div className="d-flex align-items-center justify-content-center vh-100">
                 <div className="row g-5 mx-2">
                     <div className="col text-center">
-                        <h1 className="display-1">{t('name')}</h1>
+                        <h1 className="headline-one">{t('name')}</h1>
                         <h2 className="lead">{t('title')}</h2>
-                        <a href="#" className="btn btn-learn px-5 py-3">{t('learnMore')}</a>
+                        <br></br>
+                        <button className="btn-learn px-5 py-3" onClick={() => {}}>{t('learnMore')}</button>
                     </div>
                 </div>
             </div>
