dc9f125c3f30e4f11266bd2732f595f812b49a53
Charlotte Stone <144109245+Terafora@users.noreply.github.com>
2024-07-16 16:19:08 +0100

    Create seperate translation card component

---
+++ src/components/TranslationCard.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+
+const TranslationCard = ({ translation }) => {
+    const { t } = useTranslation();
+
+    return (
+        <div className="card col-lg-3 col-md-6 col-sm-9">
+            <img src={translation.imgSrc} className="Recent-Work-Img" alt={translation.altText} />
+            <h4 className="text-center">{t(`translations.${translation.id}.title`)}</h4>
+            <a href={translation.url} className="btn btn-primary">{t('learnMore')}</a>
+        </div>
+    );
+};
+
+export default TranslationCard;
