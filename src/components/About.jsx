d99ef245a0ea0a8f4837da1ac85b08f9f2c7da79
Charlotte Stone <144109245+Terafora@users.noreply.github.com>
2024-09-20 12:04:36 +0100

    Fix layout of About section on bigger screens

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,22 +6,26 @@ export default function About() {
 
     return (
         <section className="row about">
-            <div className="row g-5 mx-2">
-            <h2>{t('about')}</h2>
-                <div className="col-12 col-lg-6">
-                    <div className="about-bg-layer1">
-                        <div className="about-bg-layer2">
-                            <div className="about-bg-layer3">
-                                <h3>{t('hello')}</h3>
-                                <p>{t('aboutText1')}</p>
-                                <p>{t('aboutText2')}</p>
+            <div className="row g-5 mx-3">
+                <h2 className="my-5">{t('about')}</h2>
+                <div className="about-bg-layer1">
+                    <div className="about-bg-layer2">
+                        <div className="about-bg-layer3">
+                            <div className="row">
+                                <div className="col-12 col-lg-6 d-flex justify-content-center align-items-center">
+                                    <div>
+                                        <h3>{t('hello')}</h3>
+                                        <p>{t('aboutText1')}</p>
+                                        <p>{t('aboutText2')}</p>
+                                    </div>
+                                </div>
+                                <div className="col-12 col-lg-6 d-flex justify-content-center align-items-center">
+                                    <img src="./pictures/CharlotteStone.jpeg" className="img-fluid about-img" alt="Charlotte Stone" />
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-<div className="col-12 col-lg-6 d-flex justify-content-center align-items-center">
-    <img src="./pictures/CharlotteStone.jpeg" className="img-fluid about-img" alt="Charlotte Stone" />
-</div>
             </div>
         </section>
     );
