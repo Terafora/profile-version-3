51955843f5f308b417a5ab1494db9d96822e0d43
Charlotte Stone <144109245+Terafora@users.noreply.github.com>
2024-09-20 11:37:07 +0100

    Update background for about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,9 +9,15 @@ export default function About() {
             <div className="row g-5 mx-2">
             <h2>{t('about')}</h2>
                 <div className="col-12 col-lg-6">
-                    <h3>{t('hello')}</h3>
-                    <p>{t('aboutText1')}</p>
-                    <p>{t('aboutText2')}</p>
+                    <div className="about-bg-layer1">
+                        <div className="about-bg-layer2">
+                            <div className="about-bg-layer3">
+                                <h3>{t('hello')}</h3>
+                                <p>{t('aboutText1')}</p>
+                                <p>{t('aboutText2')}</p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
 <div className="col-12 col-lg-6 d-flex justify-content-center align-items-center">
     <img src="./pictures/CharlotteStone.jpeg" className="img-fluid about-img" alt="Charlotte Stone" />
